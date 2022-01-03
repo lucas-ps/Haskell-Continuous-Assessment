64c7ca23910837c07e782a3f6c44b141ecf385d7
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2022-01-03 20:37:05 +0000

    Worked on main, currently not working

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,18 @@ module Main where
 
 import System.Environment
 import Compiler
+import Interpreter
 
---TODO Task 3.4
 main :: IO ()
-main = putStrLn "Implement"
+main = do
+    args <- getArgs
+    if length args == 0
+        then print $ "Error: No arguments provided"
+    else if length args > 1
+        then print $ "Error: More than one argument provided"
+    else 
+        let command = head args
+        in print(command)
+
+    -- todo: get read working, currently returns io error? not sure how to fix or whether read is the correct thing to use
+    -- runhaskell -isrc "/Users/lucas/OneDrive - University of Exeter/Year 2/CL and R/Coursework/app/Main.hs" "Assign /"x/" (Plus (N 5) (N 3))"`
